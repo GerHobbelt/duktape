0126ac15eaedbe6840814c24dd9fdedda04c80d7
Jeremy Hsu <jeremy.hsu@mimik.com>
2020-01-09 13:02:17 -0800

    added v2.5.0

--- mimik/duktape/duk_module_node.h
+++ mimik/duktape/duk_module_node.h
@@ -2,13 +2,16 @@
 #define DUK_MODULE_NODE_H_INCLUDED
 
 #include "duktape.h"
-#ifdef __cplusplus
-extern "C" {  // only need to export C interface if
-              // used by C++ source code
+
+#if defined(__cplusplus)
+extern "C" {
 #endif
+
 extern duk_ret_t duk_module_node_peval_main(duk_context *ctx, const char *path);
 extern void duk_module_node_init(duk_context *ctx);
-#endif  /* DUK_CONSOLE_H_INCLUDED */
-#ifdef __cplusplus
+
+#if defined(__cplusplus)
 }
+#endif  /* end 'extern "C"' wrapper */
+
 #endif  /* DUK_MODULE_NODE_H_INCLUDED */
